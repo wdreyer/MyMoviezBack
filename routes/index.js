6e9fddfdd3c27b614c1584867a466ea9006ac446
Your name <dreyer.wil@gmail.com>
2023-02-01 11:46:44 +0100

    update apiKEy

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch');
 const API_KEY = process.env.API_KEY;
 
 router.get('/movies', (req, res) => {
-  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=851b5521381925c280f834619ed8c453`)
  .then(response => response.json())
  .then(data => {
   console.log(data)
